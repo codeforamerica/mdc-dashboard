eceff231d904c579c69f0f4e31658ec4592ab5a0
phiden <sdengo@codeforamerica.org>
2015-01-14 16:51:18 -0800

    UI Progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,22 @@ $(document).ready(function() {
 	
 	console.log("hello world");
 	
+	//smart radio buttons
+	$('.identifier').click(function(e) {
+		
+		e.preventDefault();
+		
+		//toggle UI
+		//pass ID to the rest of the form
+		var id = $(this).attr('id');
+		console.log('id:', id);
+		
+		setForm(id);
+		
+	})
+	
+	function setForm(id) {
+		
+		$('#identifier-header').text(id);
+	}
 });
